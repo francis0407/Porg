369e406b8c23e978f4d508a35c31dee4aa695263
francis0407 <hanmingcong123@hotmail.com>
2018-07-17 16:29:16 +0800

    Add run() & REGISTER_REDUCER()

--- MapReduce/Reducer.h
+++ MapReduce/Reducer.h
@@ -5,16 +5,45 @@
 namespace mapreduce {
 
 using std::list;
-
+using std::string;
 template<class K1,class V1,class K2,class V2>
 class Reducer {
  public：
-  list<K1> keys;
-  list<V1> values;
+  list<K1>& keys;
+  list<list<V1>>& values;
   Context<K2,V2> context;
+  Reducer(list<K1>& k,list<list<V1>>& v):keys(k),values(v);
   virtual ~Reducer();
-  virtual int reduce(K1 key,List<V1> value) = 0;
+  virtual int reduce(const K1& key,const list<V1>& value) = 0;
+  virtual int run(){
+    //for reference only
+    while(!keys.empty() && !values.empty()){
+      ASSERT(reduce(keys.front(),values.front()));
+      keys.pop_front();
+      values.pop_front();
+    }
+  } 
   
 };
+
 }
-#endif
+#define  REGISTER_REDUCER(R,K1,V1,K2,V2)\
+int doReducer(list<string>& input,string& result,string& errorMessage){\
+  int len = input.size();\
+  Context<K1,list<V1>> temp,target;\
+  ADDERROR(len);\
+  ADDERROR(target.deserialize(input.front()));\
+  input.pop_front();\
+  while(!input.empty()){\
+    ADDERROR(temp.deserialize(input.front()));\
+    ADDERROR(merge_list<K1,V1>(temp.keys,temp.values,target.keys,target.values));\
+    input.pop_front();\
+    ADDERROR(temp.clear());\
+  }\
+  R reducer(target.keys,target.values);\
+  ADDERROR(reducer.run());\
+  ADDERROR(reducer.context.serialize(result));\
+  return 1;\
+}
+
+#endif //MAPREDUCE_REDUCER
